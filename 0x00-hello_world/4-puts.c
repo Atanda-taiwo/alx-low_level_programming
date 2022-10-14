f73d9c6e0739c46ccb6cf9bf4b880fb8b577fac9
Atanda-taiwo <atandataiwoazeez.com>
2022-10-13 19:40:43 -0700

    task 1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ * task 4: print using puts
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
